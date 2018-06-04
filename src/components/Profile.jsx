d4c6cb3d272f2018be1075785fb9e70c76e91055
Aleksandar Ristic <storiaca@gmail.com>
2018-06-04 14:35:35 +0200

    Api setup

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 class Profile extends Component {
+	componentDidMount() {
+		let header = new Headers({"Content-Type":"application/json", "Authorization":"token 9addac14c3f104f90331c6e3f3a41a8dfc8cd5e0"});
+		fetch('https://api.github.com/users/storiaca', {
+			method: 'GET',
+			headers: header
+    })
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+    })
+    .catch(error => console.log(error));
+	}
   render() {
 		return (
 			<div>
